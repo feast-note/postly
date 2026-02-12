e459837438344d780a49439db95bbf6b3dd764b1
heywon0909 <hw731911@gmail.com>
2026-02-12 20:50:02 +0900

    feat: pin page 컴포넌트 구조 변경

---
+++ src/components/Board.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { useDraggable } from "@/hooks/useDraggable";
+import { Post } from "@/service/post";
+import { useEffect, useRef, useState } from "react";
+import PostCard, { PostCardRef } from "./PostCard";
+import PostSetting from "./PostSetting";
+import Canvas from "./Canvas";
+
+export default function Board() {
+  const [posts, setPosts] = useState<Array<Post>>([
+    // {
+    //   id: "0",
+    //   title: "Zoom Test",
+    //   content: "확대해도 잘 움직이나요?",
+    //   zIndex: 1,
+    //   color: "#50d71e",
+    // },
+    // {
+    //   id: "1",
+    //   title: "배가 고픈가",
+    //   content: "재료는 파가 없어...",
+    //   zIndex: 0,
+    //   color: "#ffd230",
+    // },
+    // {
+    //   id: "2",
+    //   title: "Zoom Test",
+    //   content: "확대해도 잘 움직이나요?",
+    //   zIndex: 1,
+    //   color: "#50d71e",
+    // },
+    // {
+    //   id: "3",
+    //   title: "배가 고픈가",
+    //   content: "재료는 파가 없어...",
+    //   zIndex: 0,
+    //   color: "#ffd230",
+    // },
+  ]);
+  useEffect(() => {
+    fetch("/api/post")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("dat", data);
+        setPosts(data);
+      });
+  }, []);
+
+  const [selected, setSelected] = useState<string | null>(null);
+
+  const [position, setPosition] = useState({ x: 0, y: 0 }); // 캔버스 위치
+  const [scale, setScale] = useState(1); // 확대 배율
+
+  const { initCanvasPosition, initPostPosition, stopDrag, calculateDragMove } =
+    useDraggable(scale);
+
+  const postsRef = useRef<Map<string, PostCardRef>>(new Map());
+
+  const onZoom = (e: React.WheelEvent) => {
+    e.preventDefault();
+    const zoomDirection = e.deltaY > 0 ? -0.1 : 0.1;
+    const newScale = Math.min(Math.max(scale + zoomDirection, 0.1), 5);
+    setScale(newScale);
+  };
+
+  const onCanvasMouseDown = (e: React.MouseEvent) =>
+    initCanvasPosition(e, position);
+
+  const onPostMouseDown = (e: React.MouseEvent, id: string) => {
+    e.preventDefault();
+
+    const target = postsRef.current.get(id);
+
+    if (!target) return;
+
+    const currentPostPosition = target.getInitialPosition(e);
+
+    initPostPosition(e, id, currentPostPosition);
+  };
+
+  const onMouseMove = (e: React.MouseEvent) => {
+    const movement = calculateDragMove(e);
+
+    if (!movement) return;
+    const { type, x, y } = movement;
+
+    // A. 캔버스 이동 (Pan)
+    if (type === "CANVAS") {
+      setPosition({
+        x,
+        y,
+      });
+    }
+
+    // B. 노트 이동 (Move Note)
+    if (type === "POST") {
+      const el = e.target as HTMLElement;
+      const target = postsRef.current.get(el.id || el.parentElement!.id);
+      if (!target) return;
+
+      target.setPosition(x, y);
+    }
+  };
+
+  const onOutlineClick = (e: React.MouseEvent) => {
+    const el = e.target as HTMLElement;
+    setSelected(el.id || el.parentElement!.id);
+  };
+
+  return (
+    <section
+      className="w-screen h-[95vh] overflow-hidden bg-[#333]"
+      onWheel={onZoom}
+      onMouseDown={onCanvasMouseDown}
+      onMouseMove={onMouseMove}
+      onMouseUp={stopDrag}
+      onMouseLeave={stopDrag}
+      onClick={onOutlineClick}
+    >
+      <PostSetting selected={posts.find((v) => v.id === selected) || null} />
+      <Canvas position={{ x: position.x, y: position.y }} scale={scale}>
+        {posts.map((post) => (
+          <PostCard
+            {...post}
+            key={post.id}
+            ref={(el) => {
+              if (el) postsRef.current.set(post.id, el);
+            }}
+            selected={post.id === selected ? true : false}
+            onMouseDown={(e) => onPostMouseDown(e, post.id)}
+          />
+        ))}
+      </Canvas>
+    </section>
+  );
+}
