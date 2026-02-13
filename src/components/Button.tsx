58800770c9a34464af53181cf1effc6498937aa4
heywon0909 <hw731911@gmail.com>
2026-02-13 18:51:34 +0900

    feat: button UI 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ export default function Button({ value, ...props }: Props) {
   return (
     <button
       {...props}
-      className="text-neutral-200 p-2 rounded-md min-w-16 border hover:bg-gray-950 border-gray-400"
+      className="text-neutral-200 p-1 rounded-md min-w-16 border hover:bg-gray-950 border-gray-400"
     >
       {value}
     </button>
