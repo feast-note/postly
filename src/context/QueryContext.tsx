4084007c2b12e66f06867fc57e9b9c7d29109e91
heywon0909 <hw731911@gmail.com>
2026-02-20 10:31:29 +0900

    feat: tanstack query context 생성

--- src/context/QueryContext.tsx
+++ src/context/QueryContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,6 +17,9 @@ type Props = {
 
 export default function QueryContext({ children }: Props) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      {children}
+    </QueryClientProvider>
   );
 }
