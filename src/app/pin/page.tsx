606d19ea35fd67397dc3b82647f2df2f9c0f1900
heywon0909 <hw731911@gmail.com>
2026-02-25 14:38:35 +0900

    feat: pin page user 없을 시 redirect

--- src/app/pin/page.tsx
+++ src/app/pin/page.tsx
@@ -2,8 +2,16 @@ import Board from "@/components/Board";
 import { AddModeProvider } from "@/context/AddModeContext";
 import { DragModeProvider } from "@/context/DragModeContext";
 import { TransformProvider } from "@/context/TransformContext";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function PinPage() {
+export default async function PinPage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) redirect("/");
+
   return (
     <TransformProvider>
       <DragModeProvider>
