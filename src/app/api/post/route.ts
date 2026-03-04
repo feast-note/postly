2c0012b7acdd49c85e408f0d7c2da88e1165b649
heywon0909 <hw731911@gmail.com>
2026-03-04 14:44:27 +0900

    feat: post api 추가

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,16 +1,36 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
-import { getPostsByUsername } from "@/service/post";
+import { createPost, getPostsByUsername } from "@/service/post";
 import { NextResponse } from "next/server";
+import { Post } from "@/model/post";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
   const user = session?.user;
 
-  console.log("user", user);
   if (!user) {
     return new Response("Authentication Error", { status: 401 });
   }
 
   return getPostsByUsername(user.name).then((res) => NextResponse.json(res));
+}
+
+export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+
+  const { post } = await request.json();
+
+  console.log("post", post);
+  const { width, height } = post as Post;
+
+  return createPost({
+    userId: user.id,
+    width,
+    height,
+  }).then((res) => NextResponse.json(res));
 }
--- src/hooks/usePostData.tsx
+++ src/hooks/usePostData.tsx
@@ -1,10 +1,31 @@
 "use client";
-
-import { Post } from "@/model/post";
-import { useQuery } from "@tanstack/react-query";
+import { usePostPosition } from "@/context/PositionContext";
+import { Position, Post } from "@/model/post";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
+async function addPost(
+  post: Omit<Post, "id"> & { position: Position },
+): Promise<string> {
+  return fetch("/api/post", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      post: {
+        width: post.width,
+        height: post.height,
+        color: post.color,
+        zIndex: post.zIndex,
+      },
+    }),
+  }).then((res) => res.json());
+}
 export const usePostData = () => {
+  const queryClient = useQueryClient();
+  const { updatePosition } = usePostPosition();
+
   const { data: posts } = useQuery<Array<Post>>({
     queryKey: ["posts"],
     queryFn: () =>
@@ -18,9 +39,17 @@ export const usePostData = () => {
 
   const [newPosts, setNewPosts] = useState<Array<Post>>([]);
   const totalPosts = [...(posts || []), ...newPosts];
+  const mutation = useMutation({
+    mutationFn: addPost,
+    onSuccess: (data, variables) => {
+      updatePosition(data, variables.position.x, variables.position.y);
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+  });
 
   return {
-    totalPosts,
+    posts: totalPosts,
     setNewPosts,
+    postPost: mutation,
   };
 };
