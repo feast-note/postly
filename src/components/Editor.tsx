31316f2a209719336d144cae8ad5386cb9021fc9
heywon0909 <hw731911@gmail.com>
2026-03-10 18:58:18 +0900

    feat: tiptap editor 추가

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,51 +1,72 @@
 "use client";
-import { HeadingButton } from "@/components/tiptap-ui/heading-button";
-import { EditorContent, EditorContext, useEditor } from "@tiptap/react";
-import { MarkButton } from "@/components/tiptap-ui/mark-button";
-import StarterKit from "@tiptap/starter-kit";
-import { useState } from "react";
 
-import "@/components/tiptap-node/code-block-node/code-block-node.scss";
-import "@/components/tiptap-node/paragraph-node/paragraph-node.scss";
+import { EditorContent, EditorContext, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { HeadingDropdownMenu } from "./tiptap-ui/heading-dropdown-menu";
+import { MarkButton } from "@/components/tiptap-ui/mark-button";
+import { usePostData } from "@/hooks/usePostData";
+import { useSelect } from "@/context/SelectContext";
 
 type Props = {
+  id: string;
   content?: string;
 };
 
-export default function Editor({ content }: Props) {
-  const [contentInput, setContentInput] = useState(content ?? "");
+export default function Editor({ id, content }: Props) {
+  const { selected, onSelect } = useSelect();
+
+  const { modiPostItem } = usePostData();
+  const isEdit = selected === id;
+
   const editor = useEditor({
     extensions: [StarterKit],
-    content,
-    // Don't render immediately on the server to avoid SSR issues
+    content: content,
     immediatelyRender: false,
+    onBlur: ({ editor }) => {
+      const text = editor.getHTML();
+      if (content === text) return;
+      modiPostItem.mutate({ id, post: { content: text } });
+    },
+    onFocus: () => onSelect(id),
   });
 
   return (
     <EditorContext.Provider value={{ editor }}>
-      <HeadingButton
-        editor={editor}
-        level={1}
-        text="Heading 1"
-        hideWhenUnavailable={true}
-        showShortcut={true}
-        // onToggled={(e) => console.log("엥", e)}
-      />
-      <HeadingButton
-        editor={editor}
-        level={2}
-        text="Heading 2"
-        hideWhenUnavailable={true}
-        showShortcut={true}
-        // onToggled={() => console.log(`Heading ${level} toggled!`)}
-      />
+      <div className="p-4 flex-1 flex flex-col">
+        <EditorContent
+          editor={editor}
+          className="flex-1 flex flex-col"
+          role="presentation"
+          content={content}
+          id="editor"
+          onMouseDown={(e) => e.stopPropagation()}
+          onClick={() => {
+            editor?.commands.focus();
+          }}
+          autoFocus
+        />
 
-      <EditorContent
-        editor={editor}
-        role="presentation"
-        content={content}
-        onMouseDown={(e) => e.stopPropagation()}
-      />
+        <div className={`flex ${isEdit ? "visible" : "invisible"}`}>
+          <HeadingDropdownMenu
+            editor={editor}
+            levels={[1, 2, 3, 4, 5, 6]}
+            hideWhenUnavailable={true}
+            portal={false}
+          />
+          <MarkButton
+            editor={editor}
+            type="bold"
+            text="Bold"
+            hideWhenUnavailable={true}
+            showShortcut={true}
+            onToggled={() => console.log("Mark toggled!")}
+          />
+          <MarkButton type="italic" />
+          <MarkButton type="strike" />
+          <MarkButton type="code" />
+          <MarkButton type="underline" />
+        </div>
+      </div>
     </EditorContext.Provider>
   );
 }
