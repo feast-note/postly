b91a6942fa893e10125d9b022ddb20005634f143
heywon0909 <hw731911@gmail.com>
2026-03-11 16:23:23 +0900

    feat: post resizer logic 및 ui 작업

---
+++ src/components/Resizer.tsx
@@ -0,0 +1,48 @@
+import { ResizeDirection } from "@/hooks/useResize";
+
+type Props = {
+  onResize: (e: React.MouseEvent, direction: ResizeDirection) => void;
+};
+export default function Resizer({ onResize }: Props) {
+  return (
+    <>
+      <>
+        {/* 변 (Edges) */}
+        <div
+          className="absolute cursor-nwse-resize p-2 z-50"
+          onMouseDown={(e) => onResize(e, "nw")}
+        />
+        <div
+          className="right-0 absolute cursor-nesw-resize p-2 z-50"
+          onMouseDown={(e) => onResize(e, "ne")}
+        />
+        <div
+          className="bottom-0 right-0 absolute cursor-nwse-resize p-2 z-50"
+          onMouseDown={(e) => onResize(e, "se")}
+        />
+        <div
+          className="bottom-0 left-0 absolute cursor-nesw-resize p-2 z-50"
+          onMouseDown={(e) => onResize(e, "sw")}
+        />
+
+        {/* 모서리 (Corners) */}
+        <div
+          className="absolute p-2 cursor-n-resize w-full"
+          onMouseDown={(e) => onResize(e, "n")}
+        />
+        <div
+          className="absolute left-0 p-2 cursor-w-resize h-full"
+          onMouseDown={(e) => onResize(e, "w")}
+        />
+        <div
+          className="absolute right-0 p-2 cursor-e-resize h-full"
+          onMouseDown={(e) => onResize(e, "e")}
+        />
+        <div
+          className="absolute bottom-0 p-2 cursor-s-resize w-full"
+          onMouseDown={(e) => onResize(e, "s")}
+        />
+      </>
+    </>
+  );
+}
