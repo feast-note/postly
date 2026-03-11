02c8113cdc7d1739cb71bd100b45511502689847
heywon0909 <hw731911@gmail.com>
2026-03-11 17:20:53 +0900

    feat: layout css, tiptap styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import AuthContext from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
 import QueryContext from "@/context/QueryContext";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: {
@@ -12,13 +13,15 @@ export const metadata: Metadata = {
   description: "create post in infinite Canvas, infinite Ideas",
 };
 
+const inter = Inter({ subsets: ["latin"], display: "swap" });
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <AuthContext>
         <body className={`w-full overflow-auto flex flex-col`}>
           <header className="w-full flex justify-center bg-gray-900">
