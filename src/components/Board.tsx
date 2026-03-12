939919a9ec139d78c8c9cab4e26fc1aa66938c18
heywon0909 <hw731911@gmail.com>
2026-03-12 19:10:38 +0900

    feat: react-toastify 라이브러리 추가

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,22 +6,21 @@ import { useBoadInteraction } from "@/hooks/useBoardInteraction";
 import { usePostData } from "@/hooks/usePostData";
 import { Post } from "@/model/post";
 import dynamic from "next/dynamic";
+import { ToastContainer } from "react-toastify";
 
 const PostCards = dynamic(() => import("./PostCards"), { ssr: false });
 
 type Props = {
   initialData?: Post[];
 };
+
 export default function Board({ initialData }: Props) {
   const { posts } = usePostData(initialData ?? []);
 
   const { refRegister, handlers, onPostMouseDown } = useBoadInteraction();
 
   return (
-    <section
-      className={`relative w-screen h-full overflow-hidden bg-[#333]`}
-      {...handlers}
-    >
+    <section className={viewportStyle} {...handlers}>
       <ToolBar />
       <Canvas>
         {posts && (
@@ -32,6 +31,9 @@ export default function Board({ initialData }: Props) {
           />
         )}
       </Canvas>
+      <ToastContainer theme="dark" />
     </section>
   );
 }
+
+const viewportStyle = "relative w-screen h-full overflow-hidden bg-[#333]";
